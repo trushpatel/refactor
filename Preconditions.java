af17f385454fc32c0119939c4c3a49c710f827c3
Trush Patel <trushpatel@Trushs-MBP.fios-router.home>
2021-10-19 15:02:39 -0400

    refactor

--- Preconditions.java
+++ Preconditions.java
@@ -2,20 +2,24 @@ import java.util.regex.*;
 
 class Preconditions {
 
+    public static boolean check () {
+        if (pC1 && pC2 && pC3) return true;
+        return false;
+    }
     // The new name cannot be illegal
 
-    public static boolean pC1 (String newName) {
+    private static boolean pC1 (String newName) {
         Pattern p = Pattern.compile("^[_$A-Za-z][_$a-zA-Z0-9]*+");
         return p.matcher(newName).matches();
     }
 
     // The target method cannot be a duplicate with any existing method after rename
 
-    public static boolean pC2 () {}
+    private static boolean pC2 () {}
 
-    public static boolean methodSignature(String method) {}
+    private static boolean methodSignature(String method1, String method 2) {}
 
     // Rename all overriding and overridden methods
 
-    public static boolean pC3 () {}
+    private static boolean pC3 () {}
 }
