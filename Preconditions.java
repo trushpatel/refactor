8bc153788c7fd0d09e5c599862c616a2075fa639
Trush Patel <tp966666@wcupa.edu>
2021-10-19 15:40:46 -0400

    refactor

--- Preconditions.java
+++ Preconditions.java
@@ -1,11 +1,12 @@
 import java.util.regex.*;
+import java.util.*;
 
 class Preconditions {
 
-    public static boolean check () {
+    /*public static boolean check () {
         if (pC1 && pC2 && pC3) return true;
         return false;
-    }
+    }*/
 
     // The new name cannot be illegal
 
@@ -16,13 +17,20 @@ class Preconditions {
 
     // The target method cannot be a duplicate with any existing method after rename
 
-    private static boolean pC2 () {}
+//    private static boolean pC2 () {}
 
     private static boolean methodSignature(String method1, String method2) {
-        
+        String [] a1 = method1.split(" ");
+        String [] a2 = method2.split(" ");
+        System.out.println(Arrays.toString(a1));
+        System.out.println(Arrays.toString(a2));
+        return false;
     }
 
+    public static void main(String[] args) {
+        methodSignature("public void x()", "public void y()");
+    }
     // Rename all overriding and overridden methods
 
-    private static boolean pC3 () {}
+    //private static boolean pC3 () {}
 }
---
+++ methodSignature.sh
@@ -0,0 +1,2 @@
+#/bin/bash
+sed -i '' 's/x/y/' A.java
