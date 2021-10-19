8e603c4dc72b8c52af64a34ded74b392b4d6f044
Trush Patel <trushpatel@Trushs-MBP.fios-router.home>
2021-10-19 15:00:04 -0400

    refactor

--- A.java
+++ A.java
@@ -1,12 +1,4 @@
-/*
-
-    Preconditions for RiMr:
-        1. The new name cannot be illegal
-        2. The target method cannot be duplicate with any existing method after rename
-        3. An overriding/overridden method is not eligible for the target method (rename all overriding/overridden methods)
-
-*/
-
 public class A {
     private void x() {}
+    public void y() {}
 }
--- Preconditions.java
+++ Preconditions.java
@@ -1,8 +1,21 @@
 import java.util.regex.*;
 
 class Preconditions {
+
+    // The new name cannot be illegal
+
     public static boolean pC1 (String newName) {
         Pattern p = Pattern.compile("^[_$A-Za-z][_$a-zA-Z0-9]*+");
         return p.matcher(newName).matches();
     }
+
+    // The target method cannot be a duplicate with any existing method after rename
+
+    public static boolean pC2 () {}
+
+    public static boolean methodSignature(String method) {}
+
+    // Rename all overriding and overridden methods
+
+    public static boolean pC3 () {}
 }
